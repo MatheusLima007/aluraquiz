3b70331de9f47173cf5dc2e26d5657fe68ea2cd8
Matheus Lima <matheu9832@gmail.com>
2021-01-26 10:56:01 -0300

    codigo da primeira aula da imersao react

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,51 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+import QuizBackground from '../src/components/QuizBackground'
+import Widget from '../src/components/Widget'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
+// `;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+    return (
+      <QuizBackground backgroundImage={db.bg}>
+        <QuizContainer>
+          <Widget>
+            <Widget.Header>
+              <h1>The Legend</h1>
+            </Widget.Header>
+            <Widget.Content>
+              
+              <p>Paragrafo unico...</p>
+            </Widget.Content>
+          </Widget>
+
+          <Widget>
+            <Widget.Content>
+              <h1>The Legend</h1>
+              <p>Paragrafo unico...</p>
+            </Widget.Content>
+          </Widget>
+          <Footer />
+        </QuizContainer>
+        <GitHubCorner />
+      </QuizBackground>
+    )
 }
